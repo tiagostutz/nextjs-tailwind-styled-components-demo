8019968c1feee39440463bd503d83f3bcabe9337
Tiago Stutz <tiagostutz@gmail.com>
2021-07-05 23:16:23 -0300

    mobile: implement mobile version. Not ideal implementation, but a good one to show how to work on prototyping and at the same time implementing. closes #12

--- react-app/pages/compare/ComparisionTableMobile.js
+++ react-app/pages/compare/ComparisionTableMobile.js
@@ -53,6 +53,7 @@ export default function ComparisionTableMobile() {
           <FontAwesomeIcon icon={faCaretSquareDown} width="24" />
         </H1Mobile>
       </div>
+      {/* The selection of products is in this toggled panel */}
       {panelVisible && (
         <ul className="p-2 border border-t-0 border-gray-200">
           {allProducts.map((product) => (
@@ -67,6 +68,72 @@ export default function ComparisionTableMobile() {
           ))}
         </ul>
       )}
+
+      {/* Product Cards Slider */}
+      <ul className="hs full no-scrollbar mt-2">
+        {selectedProducts.map((product) => (
+          <li className="item" key={product.sku}>
+            <div className="shadow-lg p-4">
+              <div className="flex-grow">
+                {/* Basic info and picture */}
+                <ProductBasicInfo
+                  product={product}
+                  onRemove={() => unselectProduct(product)}
+                />
+              </div>
+
+              {/* Badges */}
+              <div className="pb-4">
+                <div className="flex flex-row justify-center">
+                  {badges
+                    .find((b) => b.sku === product.sku)
+                    ?.urls.map((url, idx3) => (
+                      <div className="mr-2" key={idx3}>
+                        {/* Give some space between the badges */}
+                        <ResilientImage
+                          key={idx3}
+                          src={url}
+                          width="32"
+                          height="32"
+                          alt="Badge"
+                        />
+                      </div>
+                    ))}
+                </div>
+              </div>
+
+              {/* Attributes */}
+              <ul>
+                {/* WARNING: I didn't have time to adjust the data structure for this "card-like" approach
+                so I have to make this transformations (filter, find, ...) inline */}
+                {attributesValuesWithDifferenceMark
+                  .filter((avd) =>
+                    avd.values.find((av) => av.sku === product.sku)
+                  )
+                  ?.map((attribute, idx2) => (
+                    <li key={idx2} className="text-center">
+                      <div className="border-gray-100 text-sm mt-2">
+                        {attribute.name}
+                      </div>
+                      <div
+                        className={`font-bold p-1 border-b border-gray-200 ${
+                          attribute.hasDifference ? "bg-gray-100" : ""
+                        }`}
+                      >
+                        {/* WARNING: I didn't have time to adjust the data structure for this "card-like" approach
+                            so I have to make this transformations (filter, find, ...) inline. */}
+                        {
+                          attribute.values.find((vl) => vl.sku === product.sku)
+                            ?.value
+                        }
+                      </div>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
